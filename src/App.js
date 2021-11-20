ed1bd316a046c7db1c8d4130b361b6c5421c98fb
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-20 18:04:55 +0600

    Use ui-slice to render different screens

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import styles from "./App.module.css";
-// import Startup from "./components/screens/Startup";
+import { useSelector } from "react-redux";
+import Startup from "./components/screens/Startup";
 import Game from "./components/screens/Game";
+import End from "./components/screens/End";
 import Overlays from "./components/layout/overlays/Overlays";
 
 function App() {
+  const currentScreen = useSelector((state) => state.ui.currentScreen);
   return (
     <div className={styles.App}>
       <main>
-        {/* <Startup /> */}
-        <Game />
+        {currentScreen === "start" && <Startup />}
+        {currentScreen === "game" && <Game />}
+        {currentScreen === "end" && <End />}
         <Overlays />
       </main>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store/index";
+
 import "./index.css";
 import App from "./App";
+import store from "./store/index";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
