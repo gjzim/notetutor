2c8a81803ba488b1f7c8170c8c615491a38ab896
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-21 13:01:52 +0600

    Remove Overlays component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { useSelector } from "react-redux";
 import Startup from "./components/screens/Startup";
 import Game from "./components/screens/Game";
 import End from "./components/screens/End";
-import Overlays from "./components/layout/overlays/Overlays";
 
 function App() {
   const currentScreen = useSelector((state) => state.ui.currentScreen);
   return (
-    <div className={styles.App}>
-      <main>
-        {currentScreen === "start" && <Startup />}
-        {currentScreen === "game" && <Game />}
-        {currentScreen === "end" && <End />}
-        <Overlays />
-      </main>
-    </div>
+    <>
+      {currentScreen === "start" && <Startup />}
+      {currentScreen === "game" && <Game />}
+      {currentScreen === "end" && <End />}
+    </>
   );
 }
 
