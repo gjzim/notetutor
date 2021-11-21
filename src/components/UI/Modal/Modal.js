3c76f6db247da1c50ff11faff5e0f2d3ba16df0c
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-21 13:00:45 +0600

    Use React portal to render overlays

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,22 @@
+import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 import Backdrop from "./Backdrop";
 import ModalHeader from "./ModalHeader";
 import ModalBody from "./ModalBody";
 
 function Modal(props) {
-  return (
+  return ReactDOM.createPortal(
     <>
       <div className={`${classes.Modal} ${props.className}`}>
         {props.header && <ModalHeader>{props.header}</ModalHeader>}
 
         <div className={classes.ModalContent}>
-          <ModalBody>
-            {props.children}
-          </ModalBody>
+          <ModalBody>{props.children}</ModalBody>
         </div>
       </div>
-      <Backdrop high={props.high}/>
-    </>
+      <Backdrop high={props.high} onClick={props.onBackdropClick}/>
+    </>,
+    document.getElementById("overlays")
   );
 }
 
