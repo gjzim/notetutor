e56de23870201909fa2c55adb31469bd544785bd
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-22 18:32:45 +0600

    Use component state to render modals in game and startup screens

--- src/components/screens/Game.js
+++ src/components/screens/Game.js
@@ -1,16 +1,44 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice.js";
+
 import styles from "./Game.module.css";
 import Logo from "../UI/Logo";
 import Fretboard from "../game/Fretboard";
 import Question from "../game/Question";
+import Button from "../UI/Button";
 import Clock from "../game/Clock";
+import MenuModal from "../layout/overlays/MenuModal";
+import PauseModal from "../layout/overlays/PauseModal";
+import ConfirmModal from "../layout/overlays/ConfirmModal";
 
 function Game(props) {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showPause, setShowPause] = useState(false);
+  const [showRestartConfirm, setShowRestartConfirm] = useState(false);
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.Game}>
       <Logo />
       <Fretboard />
       <Question />
+      <div className={styles.Controls}>
+        <Button onClick={() => setShowMenu(true)}>Menu</Button>
+        <Button onClick={() => setShowRestartConfirm(true)}>Restart</Button>
+        <Button onClick={() => setShowPause(true)}>Pause</Button>
+      </div>
       <Clock />
+
+      {showMenu && <MenuModal onClose={() => setShowMenu(false)} />}
+      {showPause && <PauseModal onClose={() => setShowPause(false)} />}
+      {showRestartConfirm && (
+        <ConfirmModal
+          text="Do you really want to restart the game?"
+          onReject={() => setShowRestartConfirm(false)}
+          onClose={() => setShowRestartConfirm(false)}
+        />
+      )}
     </div>
   );
 }
--- src/components/screens/Startup.js
+++ src/components/screens/Startup.js
@@ -1,18 +1,29 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice.js";
+
 import styles from "./Startup.module.css";
 import LogoLarge from "../UI/LogoLarge";
 import Button from "../UI/Button";
 import Footer from "../layout/Footer";
+import MenuModal from "../layout/overlays/MenuModal";
 
 function Startup(props) {
+  const [showMenu, setShowMenu] = useState(false);
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.Startup}>
       <LogoLarge />
       <h2 className={styles.slogan}>Master Your Fretboard</h2>
       <div className={styles.controls}>
-        <Button>Start Playing</Button>
-        <Button>Customize</Button>
+        <Button onClick={() => dispatch(uiActions.changeScreen("game"))}>
+          Start Playing
+        </Button>
+        <Button onClick={() => setShowMenu(true)}>Customize</Button>
       </div>
       <Footer />
+      {showMenu && <MenuModal onClose={() => setShowMenu(false)} />}
     </div>
   );
 }
