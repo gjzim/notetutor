159cbe9fa5ceddba19ffc727e8e5a16a523c1ded
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-23 11:53:26 +0600

    Implement the game clock component using redux

--- src/components/game/Clock.js
+++ src/components/game/Clock.js
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { gameActions } from "../../store/game-slice";
+import { formatClockTime } from "../../util";
 import styles from "./Clock.module.css";
 
 function Clock(props) {
-  return (
-    <div className={styles.Clock}>
-      0:00
-    </div>
-  );
+  const isPlaying = useSelector((state) => state.game.playing);
+  const time = useSelector((state) => state.game.instance.time);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        dispatch(gameActions.timeAddSecond());
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isPlaying, dispatch]);
+
+  return <div className={styles.Clock}>{formatClockTime(time)}</div>;
 }
 
 export default Clock;
