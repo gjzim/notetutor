9383e032c17eea8490b81e49f26433ec9e749fa4
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-29 18:14:37 +0600

    Auto hide info modals after 2000 ms

--- src/components/layout/overlays/InfoModal.js
+++ src/components/layout/overlays/InfoModal.js
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import Button from "../../UI/Button";
 import Modal from "../../UI/Modal/Modal";
 import styles from "./InfoModal.module.css";
 
-function InfoModal(props) {
+function InfoModal({ text, onClose }) {
+  useEffect(() => {
+    setTimeout(onClose, 2000);
+  }, [onClose]);
+
   return (
-    <Modal className={styles.InfoModal} onBackdropClick={props.onClose} high>
-      <p>{props.text}</p>
-      <Button type="small" onClick={props.onClose}>
+    <Modal className={styles.InfoModal} onBackdropClick={onClose} high>
+      <p>{text}</p>
+      <Button type="small" onClick={onClose}>
         Close
       </Button>
     </Modal>
