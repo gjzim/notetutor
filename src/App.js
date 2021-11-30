2dcea614e9df76c8ed422b2ccac0b7304e15dc90
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-30 15:14:59 +0600

    Load gameoptions when the App component loads

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { gameActions } from "./store/game-slice";
+import { gameDefaultOptions } from "./store/game-default-options";
+
 import Startup from "./components/screens/Startup";
 import Game from "./components/screens/Game";
 import End from "./components/screens/End";
 
-function App() {
+function App() {  
   const currentScreen = useSelector((state) => state.ui.currentScreen);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let gameOptions = window.localStorage.getItem("nt-game-options");
+    if (gameOptions) {
+      gameOptions = JSON.parse(gameOptions);
+
+      dispatch(
+        gameActions.setOptions({
+          totalQues: gameOptions.totalQues || gameDefaultOptions.totalQues,
+          strings: gameOptions.strings || gameDefaultOptions.strings,
+          frets: gameOptions.frets || gameDefaultOptions.frets,
+        })
+      );
+    }
+  }, [dispatch]);
+
   return (
     <>
       {currentScreen === "start" && <Startup />}
