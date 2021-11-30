015deff55ca3fc020e473b22a4cb226478e3fc84
Gul Jamal Zim <guljamalzim@gmail.com>
2021-11-30 14:44:43 +0600

    Update checkbox component

--- src/components/UI/Form/Checkbox.js
+++ src/components/UI/Form/Checkbox.js
@@ -3,7 +3,14 @@ import styles from "./Checkbox.module.css";
 function Checkbox(props) {
   return (
     <div className={`${styles.Checkbox} ${props.className}`}>
-      <input type="checkbox" id={props.id} name={props.name} value={props.value} />
+      <input
+        type="checkbox"
+        id={props.id}
+        name={props.name}
+        value={props.value}
+        checked={props.checked}
+        onChange={props.onChange}
+      />
       <label htmlFor={props.id}>{props.label}</label>
     </div>
   );
