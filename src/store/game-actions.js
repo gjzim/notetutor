932a614b6ff945b7b6b73849cc0efeaea3a4751b
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-04 17:55:43 +0600

    Create game redux actions file

---
+++ src/store/game-actions.js
@@ -0,0 +1,39 @@
+import { uiActions } from "./ui-slice";
+import { gameActions } from "./game-slice";
+import { generateQuiz } from "../util";
+
+export const start = () => {
+  return (dispatch, getState) => {
+    const state = getState();
+    dispatch(uiActions.changeScreen("game"));
+    dispatch(
+      gameActions.start({
+        quiz: generateQuiz(state.options),
+      })
+    );
+  };
+};
+
+export const advance = (result) => {
+  return (dispatch, getState) => {
+    const state = getState();
+    if (state.game.quiz.current.serial >= state.options.totalQues) {
+      dispatch(uiActions.changeScreen("end"));
+      dispatch(gameActions.quit());      
+    } else {
+      dispatch(
+        gameActions.advance({
+          quiz: generateQuiz(state.options, state.game.quiz.current),
+          result: result,
+        })
+      );
+    }
+  };
+};
+
+export const stop = () => {
+  return (dispatch) => {
+    dispatch(gameActions.quit());
+    dispatch(uiActions.changeScreen("start"));
+  };
+};
