0be20a3fb2fc4dfcf3ff91153c7b2ac99e1def08
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-04 17:55:20 +0600

    Update game redux slice file

--- src/store/game-slice.js
+++ src/store/game-slice.js
@@ -1,28 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const gameInitState = {
-  running: false,
-  playing: false,
-  instance: null,
-  time: 0
-};
-
 const gameSlice = createSlice({
   name: "game",
-  initialState: gameInitState,
+  initialState: {
+    playing: false,
+    quiz: null,
+    score: 0,
+    time: 0,
+  },
   reducers: {
-    start(state) {
-      state.running = true;
+    start(state, action) {
       state.playing = true;
+      state.quiz = {
+        current: action.payload.quiz,
+        prev: null,
+      };
+      state.score = 0;
+      state.time = 0;
     },
     quit(state) {
-      state = gameInitState;
+      state.playing = false;
+      state.quiz = null;
     },
     pause(state) {
       state.playing = false;
     },
     resume(state) {
       state.playing = true;
+    },
+    advance(state, action) {
+      if (action.payload.result === 'correct') {
+        state.score++;
+      }
+
+      state.quiz.prev = {
+        serial: state.quiz.current.serial,
+        string: state.quiz.current.ques.string,
+        fret: state.quiz.current.ques.fret,
+      };
+
+      state.quiz.current = action.payload.quiz;
     },
     timeAddSecond(state) {
       if (state.playing) {
