8dcdbd6a9dcb5fddddda963c4243da1254f7b488
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-04 18:06:30 +0600

    Finalize Question component

--- src/components/game/Question.js
+++ src/components/game/Question.js
@@ -5,14 +5,16 @@ function Question(props) {
   return (
     <div className={styles.Question}>
       <h2 className={styles.title}>
-        Q<span id={styles.serial}>0</span>: Select the right note{" "}
+        Q<span id={styles.serial}>{props.serial}</span>: Select the right note{" "}
       </h2>
       <ul className={styles.choices}>
-        <Choice key="0" note="A" />
-        <Choice key="1" note="B" />
-        <Choice key="2" note="A#" />
-        <Choice key="3" note="D" />
-        <Choice key="4" note="F" />
+        {props.options.map((note, index) => (
+          <Choice
+            key={index}
+            note={note}
+            onClick={props.onChoiceClick}
+          />
+        ))}
       </ul>
     </div>
   );
