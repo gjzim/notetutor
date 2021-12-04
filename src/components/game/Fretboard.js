0faa159b3b416544016a9149c19594c0c846d56d
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-04 18:05:48 +0600

    Use guitar constants to update icon in fretboard component

--- src/components/game/Fretboard.js
+++ src/components/game/Fretboard.js
@@ -1,9 +1,16 @@
+import { STRING_FRET_POSITIONS } from "../../constants/guitar";
 import styles from "./Fretboard.module.css";
 
 function Fretboard(props) {
+  const style = {
+    top: `${STRING_FRET_POSITIONS[props.string][props.fret][0]}px`,
+    left: `${STRING_FRET_POSITIONS[props.string][props.fret][1]}px`,
+  };
   return (
     <div className={styles.Fretboard}>
-      <span id={styles.quesIcon}>?</span>
+      <span id={styles.quesIcon} style={style}>
+        ?
+      </span>
     </div>
   );
 }
