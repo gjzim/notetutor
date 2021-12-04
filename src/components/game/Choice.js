374e2fdfb318ebd29c65962a58b13e5ade3c5795
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-04 18:05:16 +0600

    Add functionality to choice component

--- src/components/game/Choice.js
+++ src/components/game/Choice.js
@@ -1,7 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import TIMINGS from "../../constants/timings";
 import styles from "./Choice.module.css";
 
 function Choice(props) {
-  return <li className={styles.Choice}>{props.note}</li>;
+  const [status, setStatus] = useState("active");
+  const answer = useSelector((state) => state.game.quiz.current.answer);
+  const timeOutHandlerRef = useRef(null);
+
+  useEffect(() => {
+    return () => clearTimeout(timeOutHandlerRef.current);
+  }, []);
+
+  const handleClick = () => {
+    if (props.note === answer) {
+      setStatus("correct");
+      props.onClick("correct");
+    } else {
+      setStatus("wrong");
+      props.onClick("wrong");
+    }
+
+    timeOutHandlerRef.current = setTimeout(() => {
+      setStatus("active");
+    }, TIMINGS.SHOW_ANSWER);
+  };
+
+  let classes = [styles.Choice];
+  if (status !== "active") {
+    classes.push(styles[status]);
+  }
+
+  return (
+    <li className={classes.join(" ")} onClick={handleClick}>
+      {props.note}
+    </li>
+  );
 }
 
 export default Choice;
