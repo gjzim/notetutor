fa1b84b861dd05f0bb64e3b15cf80e924a1a37b0
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-05 17:18:50 +0600

    Remove reportWebVitals file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store/index";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,8 +14,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("app")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
