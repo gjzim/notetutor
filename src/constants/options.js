141df3a6d7c7d92e1a4ad8f26058e0b618f57f4d
Gul Jamal Zim <guljamalzim@gmail.com>
2023-02-11 10:07:29 +0600

    Convert options constants js file to ts

--- src/constants/options.js
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_OPTIONS = {
-  totalQues: 30,
-  strings: [1, 2, 3, 4, 5, 6],
-  frets: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-};
---
+++ src/constants/options.ts
@@ -0,0 +1,11 @@
+export type DefaultOptions = {
+    totalQues: number;
+    strings: number[];
+    frets: number[];
+};
+
+export const DEFAULT_OPTIONS: DefaultOptions = {
+    totalQues: 30,
+    strings: [1, 2, 3, 4, 5, 6],
+    frets: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+};
