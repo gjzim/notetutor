52791bf5dbb233dd340d4ce715ac89371cdfbb74
Gul Jamal Zim <guljamalzim@gmail.com>
2023-06-11 11:56:50 +0600

    Refactor redux store creation code

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./store/index";
+import { setupStore } from "./store";
 import { Provider } from "react-redux";
+
+const store = setupStore();
 
 ReactDOM.render(
     <React.StrictMode>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,22 @@
-import { AnyAction, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { AnyAction, configureStore, ThunkAction, combineReducers, PreloadedState } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
 import gameSlice from "./game-slice";
 import optionsSlice from "./options-slice";
 
-const store = configureStore({
-    reducer: {
-        ui: uiSlice.reducer,
-        game: gameSlice.reducer,
-        options: optionsSlice.reducer,
-    },
+const rootReducer = combineReducers({
+    ui: uiSlice.reducer,
+    game: gameSlice.reducer,
+    options: optionsSlice.reducer,
 });
 
-export type AppState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, AnyAction>;
+export function setupStore(preloadedState?: PreloadedState<AppState>) {
+    return configureStore({
+        reducer: rootReducer,
+        preloadedState: {},
+    });
+}
 
-export default store;
+export type AppState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, AnyAction>;
