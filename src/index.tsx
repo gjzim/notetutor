2fb18720618104b73323a04fe6f983cfb6a2c5bd
Gul Jamal Zim <guljamalzim@gmail.com>
2023-11-05 11:54:17 +0600

    Add redux store to Cypress global

--- src/index.tsx
+++ src/index.tsx
@@ -16,3 +16,17 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
+
+interface CypressWithStore extends Cypress.Cypress {
+    store?: typeof store;
+}
+
+declare global {
+    interface Window {
+        Cypress?: CypressWithStore;
+    }
+}
+
+if (typeof window !== "undefined" && window.Cypress) {
+    window.Cypress.store = store;
+}
